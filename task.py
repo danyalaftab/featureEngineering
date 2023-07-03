60880c719a45830ca48b521609b13e0b4a469e38
danyalaftab <93712215+danyalaftab@users.noreply.github.com>
2023-07-03 13:18:56 +0500

    Update task.py

--- task.py
+++ task.py
@@ -1,16 +1,5 @@
-from teradataml import DataFrame
-from aoa import (
-    aoa_create_context,
-)
-
-def run(**kwargs):
-    aoa_create_context()
-    # df = DataFrame.from_query("sel * from dbc.dbcinfo")
-    print("danyal")
-
-
-
-def process(**kwargs):
-    aoa_create_context()
-    # df = DataFrame.from_query("sel * from dbc.dbcinfo")
-    print("danyal")
+def evaluate():
+    print("evaluate")
+    
+def train():
+    print("train")
