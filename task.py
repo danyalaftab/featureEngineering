0d496e9a9f748285edefec073bbaf40986386274
danyalaftab <93712215+danyalaftab@users.noreply.github.com>
2023-07-05 09:42:27 +0500

    Update task.py

--- task.py
+++ task.py
@@ -1,5 +1,9 @@
-def evaluate():
-    print("evaluate")
-    
-def train():
-    print("train")
+from teradataml import DataFrame
+from aoa import (aoa_create_context, ModelContext)
+
+def run_task(context: ModelContext, **kwargs):
+    aoa_create_context()
+    df = DataFrame.from_query("sel age, count(*) as patient_cnt from pima_patient_features group by 1")
+    print(df)
+    with open(f"{context.artifact_output_path}/age_report.txt", "w") as f:
+        print(df, file=f)
